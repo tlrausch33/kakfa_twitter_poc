4eb2baf820020cd655eb4d8c4f6d501a68ed92d0
tlrausch33 <tlrausch33@gmail.com>
2017-12-13 18:21:53 -0500

    edited POC

---
+++ src/main/java/Test.java
@@ -0,0 +1,10 @@
+package kafka_twitter_poc.KafkaTwitterProducer;
+
+import com.stdatalabs.Kafka.KafkaTwitterProducer;
+
+public class Test {
+
+    com.stdatalabs.Kafka.KafkaTwitterProducer Test  = new KafkaTwitterProducer();
+
+
+}
